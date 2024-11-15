433e8efb649b0869d95895c923d51fa903875e71
ToxicalNoob <rahat3062@gmail.com>
2024-11-15 15:19:40 -0500

    implemented using framer motion the animation of close and menu button of the ham menu.

--- src/components/common/Ham.tsx
+++ src/components/common/Ham.tsx
@@ -1,13 +1,51 @@
+"use client";
 import Logo from "./Logo";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 export default function HamBurger() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Toggle menu open/close
+  const handleMenuClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="w-full p-4 font-sans font-medium flex justify-between items-center tracking-[0.35em]">
+    <div className="w-full p-4 font-sans font-medium flex justify-between items-center tracking-[0.35em] ">
       <div className="flex w-[40vw] items-center">
         <Logo />
-        <div className="h-10 border-r border-home-primary mx-4"></div>
-        <h2>MENU</h2>
+        <div className="h-10 border-r border-home-primary mx-3"></div>
+
+        {/* Smaller container */}
+        <div className="h-8 w-20 overflow-hidden relative">
+          {/* Sliding element */}
+          <motion.div
+            className="absolute top-0 left-0 w-full"
+            initial={{ y: "-2rem" }} // Start position
+            animate={{ y: isOpen ? "0rem" : "-2rem" }} // Slide up or down
+            transition={{
+              duration: 0.25, // Animation duration
+              ease: [0.175, 0.885, 0.32, 1.3], // Cubic-bezier easing
+            }}
+          >
+            <h2
+              onClick={handleMenuClick}
+              className="h-8 w-auto flex items-center justify-center "
+            >
+              CLOSE
+            </h2>
+            <h2
+              onClick={handleMenuClick}
+              className="h-8 w-auto  flex items-center justify-center"
+            >
+              MENU
+            </h2>
+          </motion.div>
+        </div>
       </div>
+
+      {/* Hire Me Text */}
       <h2>HIRE ME</h2>
     </div>
   );
