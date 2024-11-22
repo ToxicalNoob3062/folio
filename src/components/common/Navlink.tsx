d633386b94f136252305cd60f3899433ef241d63
ToxicalNoob <rahat3062@gmail.com>
2024-11-21 21:03:15 -0500

    added extra work route for testing , scaled social icons based on root font size and enabled white on hover for navlinks.

--- src/components/common/Navlink.tsx
+++ src/components/common/Navlink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NavLink({ path }: { path: string }) {
   const route = path === "home" ? "/" : `/${path}`;
   return (
-    <h1 className={`font-bold text-5xl`}>
+    <h1 className={`font-bold text-5xl hover:text-white`}>
       <Link href={route}>{path[0].toUpperCase() + path.slice(1)}</Link>
     </h1>
   );
--- src/components/common/Social.tsx
+++ src/components/common/Social.tsx
@@ -3,28 +3,23 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 
 export default function Social({ category }: { category: string }) {
-  const width = 25;
-  const height = 25;
-
   return (
     <Link href="/">
       <motion.div
         whileHover={{ scale: 1.1 }}
         transition={{ duration: 0.2 }}
-        className="relative"
+        className="relative w-8 h-8"
       >
         <Image
           src={`/${category}.png`}
           alt="Social"
-          width={width}
-          height={height}
+          layout="fill"
           className="transition-opacity duration-200 hover:opacity-0 focus:opacity-0"
         />
         <Image
           src={`/${category}-w.png`}
           alt="Social Hover"
-          width={width}
-          height={height}
+          layout="fill"
           className="absolute top-0 left-0 transition-opacity duration-200 opacity-0 hover:opacity-100 focus:opacity-100"
         />
       </motion.div>
